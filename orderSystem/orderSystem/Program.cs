d9fd2520e7ab2cc88792f183dc398485926251dc
LooN3Y <looney66@gmail.com>
2015-09-10 17:59:27 +0300

    v1
    username/pass check workong

--- orderSystem/orderSystem/Program.cs
+++ orderSystem/orderSystem/Program.cs
@@ -10,6 +10,30 @@ namespace orderSystem
     {
         static void Main(string[] args)
         {
+            string userName, pass;
+
+            Console.Write("Name: ");
+            userName = Console.ReadLine();
+
+            Console.Write("Password: ");
+            pass = Console.ReadLine();
+
+            Console.WriteLine("Name: "+userName+", Pass: "+pass+".");
+
+            string[] user = new string[2];
+            user = Credentials.Get();
+
+            if ( Equals(userName, user[0]) && Equals(pass, user[1] ) )
+            {
+                Console.WriteLine("You are in!");
+            }
+            else 
+            {
+                Console.WriteLine("No way!");
+            }    
+            
+
+            Console.ReadLine();
         }
     }
 }

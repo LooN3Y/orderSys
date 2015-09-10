fabc9e6d405964618e1202fc76b64f262d031e10
LooN3Y <looney66@gmail.com>
2015-09-10 21:57:58 +0300

    v2
    fileRead - OK
    
    ToDo: string cut + checks

--- orderSystem/orderSystem/Program.cs
+++ orderSystem/orderSystem/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace orderSystem
 {
@@ -10,29 +11,52 @@ namespace orderSystem
     {
         static void Main(string[] args)
         {
-            string userName, pass;
+            string userName, userPass, line;
+            string uName, uPass;
 
             Console.Write("Name: ");
             userName = Console.ReadLine();
 
             Console.Write("Password: ");
-            pass = Console.ReadLine();
+            userPass = Console.ReadLine();
 
-            Console.WriteLine("Name: "+userName+", Pass: "+pass+".");
+            Console.WriteLine("Name: " + userName + ", Pass: " + userPass + ".");
 
-            string[] user = new string[2];
-            user = Credentials.Get();
+            string filePath = "credentials.Evoker";
 
-            if ( Equals(userName, user[0]) && Equals(pass, user[1] ) )
+            if (!File.Exists(filePath))
             {
-                Console.WriteLine("You are in!");
+                Console.WriteLine("File not found.");
             }
-            else 
+            else
             {
-                Console.WriteLine("No way!");
-            }    
-            
+                StreamReader file = new StreamReader(filePath);
+                while ((line = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    if (line.IndexOf(":") != -1)
+                    {
+                        int labelLen = line.IndexOf(":") - 1;
+                        int dataLen = line.Length - line.IndexOf(":") - 1;
 
+
+                        if (line.Contains("username"))
+                        {
+                            // uName = ...
+                            // check userName / uName
+                        }
+                        else if (line.Contains("password"))
+                        {
+                            // uPass = ...
+                            // check userPass / uPass
+                        }
+
+                        // just for testin
+                        Console.WriteLine("Label: " + labelLen + ", Data: " + dataLen + ", break@ " + line.IndexOf(":") + " whole: " + line.Length);
+
+                    }
+                }
+            }
             Console.ReadLine();
         }
     }

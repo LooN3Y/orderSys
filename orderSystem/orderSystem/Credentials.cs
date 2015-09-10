1e98015e391b23354bb858aa35617c8c32b8d630
Surface Pro 3 <stathis@chrysikos.gr>
2015-09-10 16:02:38 +0300

    -

---
+++ orderSystem/orderSystem/Credentials.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orderSystem
+{
+    public static class Credentials
+    {
+        public string[] Get()
+        {
+            return new string[] { "admin", "password" };
+        }
+    }
+}

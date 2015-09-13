b09744a43be948354a2dd747fb66d4c5484338ac
LooN3Y <looney66@gmail.com>
2015-09-13 12:29:28 +0300

    log to file v1
    Working

--- orderSystem/orderSystem/Program.cs
+++ orderSystem/orderSystem/Program.cs
@@ -13,19 +13,20 @@ namespace orderSystem
         {
             string userName, userPass, line;
             string uName = "";
-            string uPass = "";
+            string uPass = ""; // Q
 
             Console.Write("Name: ");
             userName = Console.ReadLine();
 
             Console.Write("Password: ");
             userPass = Console.ReadLine();
-            
+
             string filePath = "credentials.Evoker";
             // File check
             if (!File.Exists(filePath))
             {
-                Console.WriteLine("File not found.");
+                Console.WriteLine("File (" + filePath + ") not found.");
+                Console.ReadLine();
             }
             // File is there
             else
@@ -53,12 +54,44 @@ namespace orderSystem
                 }
                 // ======= CHECKS
                 if (Equals(userName, uName) && Equals(userPass, uPass))
-                    Console.WriteLine("\nLogin Success!");
+                {
+                    // Log succesfull login
+                    Console.WriteLine("\n > Succesfull Login!");
+                    if (log(uName, uPass))
+                        Console.WriteLine("[Login attempt logged]");
+                    else
+                        Console.WriteLine("[Login attempt not logged]");
+                }
                 else
                     Console.WriteLine("\nGTFO!");
 
             }
             Console.ReadLine();
         }
+
+        // Login loging function ==============================
+        public static bool log(string username, string password)
+        {
+            string logFilePath = "loginLog.txt";
+            string line = "User: " + username + " (password: " + password + "), when: " + DateTime.Now + Environment.NewLine;
+
+            // First log attempt
+            if (!File.Exists(logFilePath))
+            {
+                Console.WriteLine("Log file (" + logFilePath + ") created.");
+                File.WriteAllText(logFilePath, line);
+                Console.WriteLine(line);
+                return true;
+            }
+            else if (File.Exists(logFilePath)) // Log file found
+            {
+                File.AppendAllText(logFilePath, line);
+                Console.WriteLine(line);
+                return true;
+            }
+            else // Could not create file ???????????????????? Q
+                return false;
+        }
+
     }
 }
